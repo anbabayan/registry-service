f3041d30417af2630f1c32739077c11bd4200a78
Ani Babayan <anbabayan@vmware.com>
2023-04-20 18:20:24 +0400

    first commit

---
+++ src/test/java/com/homework/registry/service/ReigistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.registry.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReigistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
